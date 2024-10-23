c004627243f29ee4801b529796cd5f2a2a0f1c7f
yarajehadrabaya <yarajehadrab@gmail.com>
2024-10-23 15:44:12 +0300

    Final result

--- final/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-
-export default defineConfig({
-  root: path.resolve(__dirname, 'final'), // حدد مسار المجلد
-  build: {
-    outDir: path.resolve(__dirname, 'dist'), // يمكنك تغيير مسار الخرج إذا رغبت
-  }
-});
